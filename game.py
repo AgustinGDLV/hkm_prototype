b055a4733c7a8212054f8a1eda8640a20860fbbb
leahwest7 <lewest@g.hmc.edu>
2025-02-16 20:04:12 -0800

    added game code

--- game.py
+++ game.py
@@ -1,9 +1,28 @@
 import random
-import timer
+import time
+from gpiozero import LED, Button
+from datetime import datetime
+import tm1637
+
 
 class Game:
     def __init__(self, time=30):
         # TODO: Assign pin IDs
+
+        r = LED(17)
+        g = LED(27)
+        b = LED(22)
+
+        # all four buttons, but change port to match breadboard
+        user1_red_button = Button(23)
+        user1_green_button = Button(24)
+        user2_red_button = Button(25)
+        user2_green_button = Button(26)
+
+        # Creating 4-digit 7-segment display object
+        tm = tm1637.TM1637(clk=17, dio=18)  # Using GPIO pins 18 and 17
+        clear = [0, 0, 0, 0]  # Defining values used to clear the display
+        # idea, display message: press button to start
 
         self.score = 0
         self.time_limit = time # in seconds
@@ -14,12 +33,20 @@ class Game:
             "blue": False,
             "green": False,
             "candy apple red": True,
+            "phoenician yellow": True,
+            "fuschia": True,
+            "maroon": False,
+
         }
 
     def do_intro(self):
-        """ Introduces game and instructions.
-            TODO: How to do this in exhibit? """
-        print("Press Y if the color is ripe and N if it is unripe!")
+        """ 
+        Game introduction for coding/debugging purposes.
+        Note that for the actual exhbit, directions will be displayed on the wood
+        """
+        print("Welcome to Ripe Reaction! The goal is to harvest more ripe cacao pods by being faster than your opponent.")
+        print("Hit Green button for ripe, red button for not ripe.")
+        print("Ripe colors: ")
 
     def display_color(self):
         """ Updates current color and display.
@@ -53,7 +80,8 @@ class Game:
                 print("*buzzer*")
             print("Score: %s\n" % self.score)
 
+
+  
 if __name__=="__main__":
-    hello_world()
     game = Game()
     game.play()
