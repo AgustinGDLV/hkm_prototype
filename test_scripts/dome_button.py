aa83b97eed287fa756b6c199cffde70eae18890a
AgustinGDLV <agustingomezdelavega@gmail.com>
2025-02-26 16:17:43 -0800

    updated button test script

--- test_scripts/dome_button.py
+++ test_scripts/dome_button.py
@@ -1,9 +1,17 @@
 from gpiozero import Button, LED
 
-button = Button(23)
+def pressed(button):
+    print("Button %d% pressed!" % button.pin.number)
+
+button1 = Button(18)
+button2 = Button(24)
+button3 = Button(25)
+button4 = Button(26)
+
+button1.when_pressed = pressed
+button2.when_pressed = pressed
+button3.when_pressed = pressed
+button4.when_pressed = pressed
 
 while True:
-    if button.is_pressed:
-        print("Button is pressed!")
-    else:
-        print("Button is not pressed.")
+    pass
